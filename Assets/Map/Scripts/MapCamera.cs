d1d83d53926c7474a80bdc50e849405d4d3bac6e
Salihin <salsatsat@gmail.com>
2021-08-28 12:18:27 +0800

    feat: Camera zoom and pan

--- Assets/Map/Scripts/MapCamera.cs
+++ Assets/Map/Scripts/MapCamera.cs
@@ -12,36 +12,36 @@ using UnityEngine;
 public class MapCamera : MonoBehaviour
 {
 	[Header("References")]
-	[SerializeField] private MapController mapController = default;
 	[SerializeField] private InputHandler inputHandler = default;
+	[SerializeField] private Transform map = default;
 	
-	[Header("UI References")]
-	[SerializeField] private Canvas canvas = default;
-
-	//[Header("Prefabs")]
-
 	[Header("Camera")]
 	[SerializeField] private Transform pivot = default;
 	public Transform Pivot { get { return pivot; } }
-	[Range(50.0f, 200.0f)]
-	[SerializeField] private float distanceToMap = 100f;
+	[SerializeField] private float minDistanceToMap = 50.0f;
+	[SerializeField] private float maxDistanceToMap = 250.0f;
+	[SerializeField] private float currDistanceToMap = 150f;
 
 	[Header("Rotation Constraints")]
-	[SerializeField] private float MinPitchAngle = 30.0f;
-	[SerializeField] private float MaxPitchAngle = 90.0f;
+	[SerializeField] private float minPitchAngle = 30.0f;
+	[SerializeField] private float maxPitchAngle = 90.0f;
 
-	[SerializeField] private float MinYawAngle = -135.0f;
-	[SerializeField] private float MaxYawAngle = 135.0f;
+	[SerializeField] private float minYawAngle = -135.0f;
+	[SerializeField] private float maxYawAngle = 135.0f;
 
-	[Header("Rotation & Zoom Scales")]
-	[SerializeField] private float HorizontalOrbitScale = 3.0f;
-	[SerializeField] private float VerticalOrbitScale = 3.0f;
+	[Header("Rotation, Zoom & Pan Scales")]
+	[Range(0.1f, 1.0f)]
+	[SerializeField] private float horizontalOrbitScale = 0.5f;
+	[Range(0.1f, 1.0f)]
+	[SerializeField] private float verticalOrbitScale = 0.5f;
+	[Range(5.0f, 25.0f)]
+	[SerializeField] private float zoomScale = 15.0f;
+	[Range(0.1f, 1.0f)]
+	[SerializeField] private float panScale = 0.5f;
 
 	// Reference to Camera component
 	private Camera cam;
 	private Transform currentPivot;
-
-	// Reference to other components
 
 	// Dragging
 	private enum DragType
@@ -53,36 +53,12 @@ public class MapCamera : MonoBehaviour
 	private DragType dragType = DragType.None;
 	private Vector3 dragWorldOrigin;
 
-	// Ground plane (for raycasting)
-	private readonly Plane plane = new Plane(Vector3.up, Vector3.zero);
-
-	public Vector2[] BoundaryPoints { get; private set; } = new Vector2[4] { Vector2.zero, Vector2.zero, Vector2.zero, Vector2.zero };
-
 	private bool needToUpdateBounds = false;
-	private bool needToUpdateViewport = false;
-	private bool needToAdjustZoom = false;
-	private Vector2 mapViewCenter = Vector2.zero;
-	private int lastPixelHeight = 0;
-
-	// Touch
-	private const float Log2 = 0.30103f;
-	private float initialZoom;
-	private float invTouchDistance;
-	private Vector2 initialTouchVector;
-	private float initialCameraYaw;
-	private Vector3 prevTouchCenterWorld;
-
-	// Bottom-left, top-left, top-right and bottom-right corners of the viewport
-	private static readonly Vector3[] FullViewportPoints = {
-		new Vector3(0, 0, 0),
-		new Vector3(0, 1, 0),
-		new Vector3(1, 1, 0),
-		new Vector3(1, 0, 0),
-	};
-	private readonly Vector3[] viewportPoints = new Vector3[4];
 
 	public delegate void OnBoundsChangeDelegate();
 	public event OnBoundsChangeDelegate OnBoundsChange;
+
+	private const float MaxDistanceToMapCentre = 100;
 
 	//
 	// Unity Methods
@@ -93,22 +69,20 @@ public class MapCamera : MonoBehaviour
 		cam = GetComponent<Camera>();
 
 		// Initialize variables
-		WrapAngle(ref MinYawAngle);
-		WrapAngle(ref MaxYawAngle);
+		WrapAngle(ref minYawAngle);
+		WrapAngle(ref maxYawAngle);
 
 		currentPivot = pivot ?? transform;
 
 		// Initialize camera position
-		UpdateDistanceToMap();
+		UpdatePosition();
 
 		needToUpdateBounds = true;
-		needToUpdateViewport = true;
 	}
 
 	private void Awake()
 	{
 		Debug.Assert(inputHandler != null, "MapCamera: Missing inputHandler");
-		Debug.Assert(mapController != null, "MapCamera: Missing mapController");
 	}
 
 	private void Start()
@@ -126,44 +100,11 @@ public class MapCamera : MonoBehaviour
 		}
 
 		// Adjust camera position for map-view-area
-		UpdateMapViewCenter();
+		UpdatePosition();
 	}
 
 	private void Update()
 	{
-		if (Input.touchCount == 2)
-		{
-			Touch t1 = Input.GetTouch(0);
-			Touch t2 = Input.GetTouch(1);
-
-			var touchCenter = (t1.position + t2.position) * 0.5f;
-
-			inputHandler.GetWorldPoint(touchCenter, out Vector3 touchCenterWorld);
-
-			if (t1.phase == TouchPhase.Began || t2.phase == TouchPhase.Began)
-			{
-				initialTouchVector = t2.position - t1.position;
-				initialCameraYaw = currentPivot.rotation.eulerAngles.y;
-
-				initialZoom = mapController.zoom;
-				invTouchDistance = 1f / (t1.position - t2.position).magnitude;
-			}
-			else
-			{
-				float distance = (t1.position - t2.position).magnitude;
-				float newZoom = initialZoom + Mathf.Log10(distance * invTouchDistance) / Log2;
-
-				var yawDiff = Angle(t2.position - t1.position, initialTouchVector) - currentPivot.rotation.eulerAngles.y + initialCameraYaw;
-
-				mapController.ChangeZoom(newZoom - mapController.zoom, touchCenterWorld.x, touchCenterWorld.z);
-				PanMap(prevTouchCenterWorld);
-				Orbit(0, yawDiff);
-				PanMap(-touchCenterWorld);
-			}
-
-			prevTouchCenterWorld = touchCenterWorld;
-		}
-
 #if UNITY_EDITOR
 		if (needToUpdateBounds || !Application.isPlaying)
 #else
@@ -171,15 +112,7 @@ public class MapCamera : MonoBehaviour
 #endif
 		{
 			needToUpdateBounds = false;
-
-			if (needToUpdateViewport)
-			{
-				needToUpdateViewport = false;
-				UpdateCameraOffset();
-			}
-
 			UpdateMapBounds();
-			needToAdjustZoom = false;
 		}
 	}
 
@@ -225,7 +158,7 @@ public class MapCamera : MonoBehaviour
 			float deltaY = inputHandler.MouseDelta.y;
 			if (Math.Abs(deltaX) > 0 || Math.Abs(deltaY) > 0)
 			{
-				Orbit(deltaY * VerticalOrbitScale, deltaX * HorizontalOrbitScale);
+				Orbit(deltaY * verticalOrbitScale, deltaX * horizontalOrbitScale);
 			}
 		}
 	}
@@ -258,20 +191,26 @@ public class MapCamera : MonoBehaviour
 	// Public Methods
 	//
 
+	public void ResetRotation()
+	{
+		currentPivot.rotation = Quaternion.Euler(90, 0, 0);
+		UpdatePosition();
+	}
 
+	public void ResetCameraOffset()
+	{
+		cam.transform.localPosition = Vector3.zero;
+	}
+
+	public void Zoom(float change)
+	{
+		currDistanceToMap = Mathf.Clamp(currDistanceToMap - change * zoomScale, minDistanceToMap, maxDistanceToMap);
+		UpdatePosition();
+	}
 
 	//
 	// Private Methods
 	//
-
-	private static float Angle(Vector2 a, Vector2 b)
-	{
-		var an = a.normalized;
-		var bn = b.normalized;
-		var x = an.x * bn.x + an.y * bn.y;
-		var y = an.y * bn.x - an.x * bn.y;
-		return Mathf.Atan2(y, x) * Mathf.Rad2Deg;
-	}
 
 	private static void WrapAngle(ref float angle)
 	{
@@ -297,13 +236,13 @@ public class MapCamera : MonoBehaviour
 
 	private void PanMap(Vector3 offset)
 	{
-		mapController.MoveInUnits(offset.x, offset.z);
-	}
+		Vector3 current = cam.transform.position;
+		Vector3 target = cam.transform.position + new Vector3(offset.x, 0.0f, offset.z);
 
-	public void ResetRotation()
-	{
-		currentPivot.rotation = Quaternion.Euler(90, 0, 0);
-		UpdatePosition();
+		Vector3 updatedCamPosition = Vector3.MoveTowards(current, target, panScale);
+		cam.transform.position = new Vector3(Mathf.Clamp(updatedCamPosition.x, map.position.x - MaxDistanceToMapCentre, map.position.x + MaxDistanceToMapCentre),
+											 updatedCamPosition.y,
+											 Mathf.Clamp(updatedCamPosition.z, map.position.z - MaxDistanceToMapCentre, map.position.z + MaxDistanceToMapCentre));
 	}
 
 	private void Orbit(float pitch, float yaw)
@@ -313,64 +252,21 @@ public class MapCamera : MonoBehaviour
 			euler.y -= 360f;
 
 		currentPivot.rotation = Quaternion.Euler(
-			Mathf.Clamp(euler.x, MinPitchAngle, MaxPitchAngle),
-			Mathf.Clamp(euler.y, MinYawAngle, MaxYawAngle),
+			Mathf.Clamp(euler.x, minPitchAngle, maxPitchAngle),
+			Mathf.Clamp(euler.y, minYawAngle, maxYawAngle),
 			0);
 
 		UpdatePosition();
 	}
 
-	private void Zoom(float change)
-	{
-		inputHandler.GetWorldPoint(Input.mousePosition, out Vector3 zoomPoint);
-		mapController.ChangeZoom(change, zoomPoint.x, zoomPoint.z);
-	}
-
 	private void UpdatePosition()
 	{
-		currentPivot.localPosition = currentPivot.forward * -distanceToMap;
+		currentPivot.localPosition = currentPivot.forward * -currDistanceToMap;
 		needToUpdateBounds = true;
-	}
-
-	private void UpdateDistanceToMap()
-	{
-		// Set camera distance so that MapTile.Size pixels represent 1 unit in world space
-		//distanceToMap = DistanceFromPixelsAndUnits(MapTile.Size, 1f / mapController.mapScale);
-		UpdatePosition();
-	}
-
-	private void UpdateMapViewCenter()
-	{
-		UpdateDistanceToMap();
-
-		needToUpdateBounds = true;
-		needToUpdateViewport = true;
-
-		if (lastPixelHeight != cam.pixelHeight)
-		{
-			lastPixelHeight = cam.pixelHeight;
-			needToAdjustZoom = true;
-		}
-	}
-
-	private void UpdateCameraOffset()
-	{
-		cam.transform.localPosition = Vector3.zero;
-
-		UpdateViewportBounds();
-	}
-
-	private void UpdateViewportBounds()
-	{
-		for (int i = 0; i < 4; i++)
-		{
-			viewportPoints[i] = FullViewportPoints[i];
-		}
 	}
 
 	private void UpdateMapBounds()
 	{
-		if (OnBoundsChange != null)
-			OnBoundsChange();
+		OnBoundsChange?.Invoke();
 	}
 }
